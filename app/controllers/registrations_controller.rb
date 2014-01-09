0f0f8c1f2d346eb978f4e054f9e99a44bacab27c
nadia12 <nadia_riantini@ymail.com>
2014-01-09 17:08:41 +0700

    form sign up admin

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    show_up_profile_path
+    show_up_profile_path resource
   end
 
   def after_update_path_for(resource)
